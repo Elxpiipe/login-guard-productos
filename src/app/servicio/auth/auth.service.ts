285e6da60afd1163e62f3512f25babbf6f8204c0
Felipe Veliz <fel.veliz@duocuc.cl>
2024-10-31 22:09:02 -0300

    Pagina simple de Login

--- src/app/servicio/auth/auth.service.ts
+++ src/app/servicio/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { CuerpoLogin } from 'src/app/interfaces/CuerpoLogin';
 import { UsuarioLogeado } from 'src/app/interfaces/UsuarioLogeado';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,12 +10,16 @@ export class AuthService {
   private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
   public usuarioLogeado: UsuarioLogeado | null = null;
   public accessToken: string | null = null;
+  // Observador de cargando
+  private $cargando = new BehaviorSubject<boolean>(false);
+  public cargando = this.$cargando.asObservable();
   constructor(
     private http: HttpClient
   ) {
     
    }
    public iniciarSesion(nombre_usuario: string, contrasenia: string){
+    this.$cargando.next(true)
     const cuerpo: CuerpoLogin = {
       username: nombre_usuario,
       password: contrasenia
@@ -27,6 +32,7 @@ export class AuthService {
     .subscribe(resultado => {
       this.usuarioLogeado = resultado;
       this.accessToken = resultado.accessToken;
+      this.$cargando.next(false)
       console.log(resultado);
     });
    }
